96001e8487eeb8d57481ccbf4190a7a83225dc08
MuhammadTalha15 <talhaz5518@gmail.com>
2025-07-02 12:42:22 +0500

    Header Design Completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,20 @@ import './style.scss'
 const Navbar = () => {
   return (
     <>
-        <header className='header'>
-            <div className="logo">
-                <h1 className="logo-txt">TrackSpace</h1>
-            </div>
-        </header>
+      <header className='header'>
+        <div className="logo">
+          <h1 className="logo-txt">TrackSpace</h1>
+        </div>
+
+        <div className="search-bar-cnt">
+          <div className='searchIco'></div>
+          <input type="text" name="search" id="search" className='searchBox' placeholder='Search Task'/>
+        </div>
+
+        <a href="#" aria-label='Setting Button' className='set-ico-a'>
+          <span className='set-ico'></span>
+        </a>
+      </header>
     </>
   )
 }
